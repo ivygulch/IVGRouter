2a0918ff188e679819bafd358ab8338017c92a1a
Douglas Sjoquist <dwsjoquist@sunetos.com>
2016-04-05 23:09:45 -0400

    Initial Commit

---
+++ IVGAppContainer/IVGAppContainer.h
@@ -0,0 +1,19 @@
+//
+//  IVGAppContainer.h
+//  IVGAppContainer
+//
+//  Created by Douglas Sjoquist on 4/5/16.
+//  Copyright © 2016 Ivy Gulch LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IVGAppContainer.
+FOUNDATION_EXPORT double IVGAppContainerVersionNumber;
+
+//! Project version string for IVGAppContainer.
+FOUNDATION_EXPORT const unsigned char IVGAppContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IVGAppContainer/PublicHeader.h>
+
+
